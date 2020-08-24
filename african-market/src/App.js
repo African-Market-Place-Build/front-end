d07dead92de061151da3465dfbb72501994fc7bb
Bender0239 <hayden.bender@protonmail.com>
2020-08-24 11:29:08 -0700

    nav done

--- african-market/src/App.js
+++ african-market/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {Link, Switch, Route} from 'react-router-dom'
+import styled from 'styled-components'
 
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  padding: 30px;
+  font-family: Verdana, Geneva, sans-serif;
+  ul {
+    display: flex;
+    width: 30%;
+    li {
+      margin-left: 5%;
+      margin-right: 5%;
+    }
+  }
+  .link {
+    color: black;
+    text-decoration: none;
+    &:hover{
+      color: tomato;
+    }
+  }
+`
 
 function App() {
+
   return (
     <div className="App">
-      <div>African Marketplace</div>
+      <StyledNav>
+      <h1>African Marketplace</h1>
+      <ul>
+        <li>
+          <Link className='link' to='/register'>Sign Up</Link>
+        </li>
+        <li>
+          <Link className='link' to='/login'>Login</Link>
+        </li>
+        <li>
+        <Link className='link' to='/market'>Market</Link>
+        </li>
+      </ul>
+      </StyledNav>
     </div>
   );
 }
