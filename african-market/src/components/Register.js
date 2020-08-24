5bf05bd7bde46a84405e586285c0e081ee7de988
Eric Rice <ericmichaelrice@protonmail.com>
2020-08-24 16:22:52 -0700

    Sign up form

---
+++ african-market/src/components/Register.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import useForm from 'react-hook-form'
+
+const SignUp = () => {
+    const {register,handleSubmit,errors} = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type='text' placeholder='Email' name='email' ref={register} />
+            <input type='password' placeholder='Password' name='password' ref={register} />
+            <input type='submit' />
+        </form>
+    )
+}
+
+export default SignUp
