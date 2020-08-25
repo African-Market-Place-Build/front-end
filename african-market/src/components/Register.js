545a9a05b8ed73fea8d8a96fde78fce4eb5fc3ba
Eric Rice <ericmichaelrice@protonmail.com>
2020-08-24 19:01:10 -0700

    Registration form

--- african-market/src/components/Register.js
+++ african-market/src/components/Register.js
@@ -1,20 +1,44 @@
 import React from 'react'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import styled from 'styled-components'
+
+
+const Form = styled.form`
+color: blue;`
 
 const SignUp = () => {
-    const {register,handleSubmit,errors} = useForm()
+    const { register, handleSubmit, errors } = useForm()
 
     const onSubmit = (data) => {
-        console.log(data)
+
+        const newUser = {
+            name: data.name,
+            email: data.email,
+            password: data.password
+        }
+        console.log(newUser)
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type='text' placeholder='Email' name='email' ref={register} />
-            <input type='password' placeholder='Password' name='password' ref={register} />
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="name">Name: </label>
+            <input type='text' placeholder='Your name' name='name' ref={register} />
+            <label htmlFor="email">Email: </label>
+            <input type='text'
+                placeholder='Email'
+                name='email' ref={register}
+                ref={register({required: true, pattern: /^\S+@\S+\.\S+$/})}/>
+            {errors.email && <p>Requires a valid email address</p>}
+            <label htmlFor="password">Password: </label>
+            <input type='password' placeholder='Password'
+                name='password'
+                ref={register({ required: true, minLength: 8 })} />
             <input type='submit' />
-        </form>
+            {errors.password && <p>Pasassword must be atleast 8 characters!</p>}
+        </Form>
+
     )
+
 }
 
 export default SignUp
