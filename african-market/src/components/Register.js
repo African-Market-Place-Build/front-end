b04fbc63693d8e6a647e2c158f3c894be3e05641
Eric Rice <ericmichaelrice@protonmail.com>
2020-08-24 20:48:47 -0700

    Login form color scheme

--- african-market/src/components/Register.js
+++ african-market/src/components/Register.js
@@ -3,8 +3,18 @@ import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
 
-const Form = styled.form`
-color: blue;`
+const LoginBox = styled.div`
+    position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0,0,0,.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+`
 
 const SignUp = () => {
     const { register, handleSubmit, errors } = useForm()
@@ -20,7 +30,8 @@ const SignUp = () => {
     }
 
     return (
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <LoginBox>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name">Name: </label>
             <input type='text' placeholder='Your name' name='name' ref={register} />
             <label htmlFor="email">Email: </label>
@@ -35,8 +46,8 @@ const SignUp = () => {
                 ref={register({ required: true, minLength: 8 })} />
             <input type='submit' />
             {errors.password && <p>Pasassword must be atleast 8 characters!</p>}
-        </Form>
-
+        </form>
+        </LoginBox>
     )
 
 }
